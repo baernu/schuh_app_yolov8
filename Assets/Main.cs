fdd8adf2149887f166c7e5a536fc2bcae7070268
Bernhard Messerli <bernhard.messerli.5@gmail.com>
2024-03-05 19:10:36 +0100

    05032024/1910

--- Assets/Main.cs
+++ Assets/Main.cs
@@ -9,50 +9,81 @@ public class Main : MonoBehaviour {
     public NNModel _model;
     public Texture2D _image;
     public UI.RawImage _imageView;
-    
+
+    [SerializeField] Vector2 webCamResolution = new Vector2(1920, 1080);
+    [SerializeField] string webCamName;
+    [SerializeField] Material material;
+    WebCamTexture webCamTexture;
+    RenderTexture inputRT;
+    Tensor output0;
+    Tensor inputTensor;
+    IWorker worker;
+
     private int _resizeLength = 640; // リサイズ後の正方形の1辺の長さ
 
     // 確認用の表示色 GBR -RGB
     private List<Color> _colorList = new List<Color>() {
-        Color.red,     // 0: 鼻(Nose)
-        Color.cyan,    // 1: 左目(Left Eye)
-        Color.blue,    // 2: 右目(Right Eye)
-        Color.cyan,    // 3: 左耳(Left Ear)
-        Color.blue,    // 4: 右耳(Right Ear)
-        Color.green,   // 5: 左肩(Left Shoulder)
-        Color.magenta, // 6: 右肩(Right Shoulder)
-        Color.green,   // 7: 左肘(Left Elbow)
-        Color.magenta, // 8: 右肘(Right Elbow)
-        Color.green,   // 9: 左手首(Left Wrist)
-        Color.magenta, // 10: 右手首(Right Wrist)
-        Color.gray,    // 11: 左腰(Left Hip)
-        Color.yellow,  // 12: 右腰(Right Hip)
-        Color.gray,    // 13: 左膝(Left Knee)
-        Color.yellow,  // 14: 右膝(Right Knee)
-        Color.gray,    // 15: 左足首(Left Ankle)
-        Color.yellow   // 16: 右足首(Right Ankle)
+        Color.red,     // 0: big toe left
+        Color.cyan,    // 1: big toe right
+        Color.blue,    // 2: little toe left
+        Color.magenta,    // 3: little toe right
+        Color.white,    // 4: heel left
+        Color.green,   // 5: heel right
+        new Color(0.5f,0.5f,0.5f,1), // 6: inside ankle left
+        new Color(0.5f,0,0.5f,1),   // 7: inside ankle right
+        new Color(0,0.5f,0.5f,1), // 8: outside ankle left
+        new Color(0,0.5f,0,1),   // 9: outside ankle right
+        new Color(0.25f,0.25f,0.5f,1), // 10: ankle kink left
+        new Color(0.75f,0.25f,0,1)    // 11: ankle kink right
+       
     };
+    
 
     // Start is called before the first frame update
     void Start() {
         // onnxモデルのロードとワーカーの作成
         var model = ModelLoader.Load(_model);
-        var worker = WorkerFactory.CreateWorker(WorkerFactory.Type.ComputePrecompiled, model);
+        worker = WorkerFactory.CreateWorker(WorkerFactory.Type.ComputePrecompiled, model);
+
+        webCamTexture = new WebCamTexture(webCamName, (int)webCamResolution.x, (int)webCamResolution.y);
+        webCamTexture.Play();
+        inputRT = new RenderTexture((int)webCamResolution.x, (int)webCamResolution.y, 0);
+
+    }
+
+    void Update()
+    {
+        if(!webCamTexture.didUpdateThisFrame) return;
+
+        var aspect1 = (float)webCamTexture.width / webCamTexture.height;
+        var aspect2 = (float)inputRT.width / inputRT.height;
+        var aspectGap = aspect2 / aspect1;
+
+        var vMirrored = webCamTexture.videoVerticallyMirrored;
+        var scale = new Vector2(aspectGap, vMirrored ? -1 : 1);
+        var offset = new Vector2((1 - aspectGap) / 2, vMirrored ? 1 : 0);
+
         
+        Graphics.Blit(webCamTexture, inputRT, scale, offset);
+        //material.mainTexture = inputRT;
+
+        _image = toTexture2D(inputRT);
+
         // モデルのinputサイズに変換し、Tensorを生成
         var texture = ResizedTexture(_image, _resizeLength, _resizeLength);
-        Tensor inputTensor = new Tensor(texture, channels: 3);
+        inputTensor = new Tensor(texture, channels: 3);
 
         // 推論実行
         worker.Execute(inputTensor);
 
         // 結果の解析
-        Tensor output0 = worker.PeekOutput("output0");
+        output0 = worker.PeekOutput("output0");
         List<DetectionResult> ditects = ParseOutputs(output0, 0.75f, 0.75f);
 
-        worker.Dispose();
+
         inputTensor.Dispose();
         output0.Dispose();
+
 
         // 結果の描画
         // 縮小した画像を解析しているので、結果を元のサイズに変換
@@ -60,37 +91,43 @@ public class Main : MonoBehaviour {
         float scaleY = _image.height / (float)_resizeLength;
         // 結果表示用に画像をコピー
         var image = ResizedTexture(_image, _image.width, _image.height);
-        foreach (DetectionResult ditect in ditects) {
+        foreach (DetectionResult ditect in ditects)
+        {
             Debug.Log($"{ditect.score:0.00}");
             // 検出した領域描画
             int x1 = (int)(ditect.x1 * scaleX);
             int x2 = (int)(ditect.x2 * scaleX);
             int y1 = (int)(ditect.y1 * scaleY);
             int y2 = (int)(ditect.y2 * scaleY);
-            for (int x = x1; x < x2; x++) {
-                image.SetPixel(x, _image.height-y1, Color.red);
-                image.SetPixel(x, _image.height-(y1-1), Color.red);
-                image.SetPixel(x, _image.height-y2, Color.red);
-                image.SetPixel(x, _image.height-(y2+1), Color.red);
+            for (int x = x1; x < x2; x++)
+            {
+                image.SetPixel(x, _image.height - y1, Color.red);
+                image.SetPixel(x, _image.height - (y1 - 1), Color.red);
+                image.SetPixel(x, _image.height - y2, Color.red);
+                image.SetPixel(x, _image.height - (y2 + 1), Color.red);
             }
-            for (int y = y1; y < y2; y++) {
-                image.SetPixel(x1, _image.height-y, Color.red);
-                image.SetPixel(x1-1, _image.height-y, Color.red);
-                image.SetPixel(x2, _image.height-y, Color.red);
-                image.SetPixel(x2+1, _image.height-y, Color.red);
+            for (int y = y1; y < y2; y++)
+            {
+                image.SetPixel(x1, _image.height - y, Color.red);
+                image.SetPixel(x1 - 1, _image.height - y, Color.red);
+                image.SetPixel(x2, _image.height - y, Color.red);
+                image.SetPixel(x2 + 1, _image.height - y, Color.red);
             }
-            
+
 
             // 検出したキーポイントを描画
             int point = 0;
-            foreach (KeyPoint kp in ditect.keypoints) {
+            foreach (KeyPoint kp in ditect.keypoints)
+            {
                 // 中心から7ピクセル範囲を描画
                 int centerX = (int)(kp.x * scaleX);
                 int centerY = (int)(kp.y * scaleY);
-                for (int x = centerX - 3; x < centerX + 3; x++) {
-                    for (int y = centerY - 3; y < centerY + 3; y++) {
+                for (int x = centerX - 3; x < centerX + 3; x++)
+                {
+                    for (int y = centerY - 3; y < centerY + 3; y++)
+                    {
                         // 検出結果は左上が原点だが、Texture2Dは左下が原点なので上下を入れ替える
-                        image.SetPixel(x, _image.height-y, _colorList[point]);
+                        image.SetPixel(x, _image.height - y, _colorList[point]);
                     }
                 }
                 point++;
@@ -98,8 +135,20 @@ public class Main : MonoBehaviour {
         }
         image.Apply();
 
-        _imageView.texture = image;
-        
+        //_imageView.texture = image;
+        material.mainTexture = image;
+
+
+
+    }
+
+    void OnDestroy()
+    {
+        if (webCamTexture != null) Destroy(webCamTexture);
+        if (inputRT != null) Destroy(inputRT);
+        worker.Dispose();
+        //inputTensor.Dispose();
+        //output0.Dispose();
     }
 
     private List<DetectionResult> ParseOutputs(Tensor output0, float threshold, float iouThres) {
@@ -191,6 +240,16 @@ public class Main : MonoBehaviour {
         RenderTexture.active = preRt;
         RenderTexture.ReleaseTemporary(rt);
         return resizedTexture;
+    }
+
+    Texture2D toTexture2D(RenderTexture rTex)
+    {
+        Texture2D tex = new Texture2D(rTex.width, rTex.height, TextureFormat.RGB24, false);
+        // ReadPixels looks at the active RenderTexture.
+        RenderTexture.active = rTex;
+        tex.ReadPixels(new UnityEngine.Rect(0, 0, rTex.width, rTex.height), 0, 0);
+        tex.Apply();
+        return tex;
     }
 
 }
