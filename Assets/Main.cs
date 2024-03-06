a4f2d9006ae58d492647ee5b867716a5aa9686d6
Bernhard Messerli <bernhard.messerli.5@gmail.com>
2024-03-06 19:31:20 +0100

    06.032024/1931

--- Assets/Main.cs
+++ Assets/Main.cs
@@ -20,9 +20,9 @@ public class Main : MonoBehaviour {
     IWorker worker;
     int count = 0;
 
-    private int _resizeLength = 640; // リサイズ後の正方形の1辺の長さ
+    private int _resizeLength = 640; 
 
-    // 確認用の表示色 GBR -RGB
+    
     private List<Color> _colorList = new List<Color>() {
         Color.red,     // 0: big toe left
         Color.cyan,    // 1: big toe right
@@ -42,7 +42,9 @@ public class Main : MonoBehaviour {
 
     // Start is called before the first frame update
     void Start() {
-        // onnxモデルのロードとワーカーの作成
+        
+        Application.targetFrameRate = 40;
+        // onnx
         var model = ModelLoader.Load(_model);
         worker = WorkerFactory.CreateWorker(WorkerFactory.Type.ComputePrecompiled, model);
 
@@ -57,7 +59,7 @@ public class Main : MonoBehaviour {
         
         if(!webCamTexture.didUpdateThisFrame) return;
 
-        if (count == 3)
+        if (count == 6)
         {
             var aspect1 = (float)webCamTexture.width / webCamTexture.height;
             var aspect2 = (float)inputRT.width / inputRT.height;
@@ -73,14 +75,14 @@ public class Main : MonoBehaviour {
 
             _image = toTexture2D(inputRT);
 
-            // モデルのinputサイズに変換し、Tensorを生成
+            
             var texture = ResizedTexture(_image, _resizeLength, _resizeLength);
             inputTensor = new Tensor(texture, channels: 3);
 
-            // 推論実行
+            
             worker.Execute(inputTensor);
 
-            // 結果の解析
+            
             output0 = worker.PeekOutput("output0");
             List<DetectionResult> ditects = ParseOutputs(output0, 0.5f, 0.5f);
 
@@ -89,20 +91,19 @@ public class Main : MonoBehaviour {
             output0.Dispose();
 
 
-            // 結果の描画
-            // 縮小した画像を解析しているので、結果を元のサイズに変換
+            
             float scaleX = _image.width / (float)_resizeLength;
             float scaleY = _image.height / (float)_resizeLength;
-            // 結果表示用に画像をコピー
+            
             var image = ResizedTexture(_image, _image.width, _image.height);
             foreach (DetectionResult ditect in ditects)
             {
-                //Debug.Log($"{ditect.score:0.00}");
-                // 検出した領域描画
+               
                 int x1 = (int)(ditect.x1 * scaleX);
                 int x2 = (int)(ditect.x2 * scaleX);
                 int y1 = (int)(ditect.y1 * scaleY);
                 int y2 = (int)(ditect.y2 * scaleY);
+                
                 for (int x = x1; x < x2; x++)
                 {
                     image.SetPixel(x, _image.height - y1, Color.red);
@@ -117,20 +118,20 @@ public class Main : MonoBehaviour {
                     image.SetPixel(x2, _image.height - y, Color.red);
                     image.SetPixel(x2 + 1, _image.height - y, Color.red);
                 }
+                
 
-
-                // 検出したキーポイントを描画
+               
                 int point = 0;
                 foreach (KeyPoint kp in ditect.keypoints)
                 {
-                    // 中心から7ピクセル範囲を描画
+                    
                     int centerX = (int)(kp.x * scaleX);
                     int centerY = (int)(kp.y * scaleY);
                     for (int x = centerX - 3; x < centerX + 3; x++)
                     {
                         for (int y = centerY - 3; y < centerY + 3; y++)
                         {
-                            // 検出結果は左上が原点だが、Texture2Dは左下が原点なので上下を入れ替える
+                            
                             image.SetPixel(x, _image.height - y, _colorList[point]);
                         }
                     }
@@ -142,24 +143,18 @@ public class Main : MonoBehaviour {
             //_imageView.texture = image;
             material.mainTexture = image;
             count = 0;
+            return;
         }
-        /*
-        else
-        {
-            material.mainTexture = inputRT;
-        }
-        */
-        count++;
+        material.mainTexture = webCamTexture;
        
-
-
+        count++;
     }
 
     void OnDestroy()
     {
         
-        //if (webCamTexture != null) Destroy(webCamTexture);
-        //if (inputRT != null) Destroy(inputRT);
+        if (webCamTexture != null) Destroy(webCamTexture);
+        if (inputRT != null) Destroy(inputRT);
         worker.Dispose();
         inputTensor.Dispose();
         output0.Dispose();
@@ -167,27 +162,24 @@ public class Main : MonoBehaviour {
 
     private List<DetectionResult> ParseOutputs(Tensor output0, float threshold, float iouThres) {
 
-        // 検出結果の行数
         int outputWidth = output0.shape.width;
         
-        // 検出結果として採用する候補
         List<DetectionResult> candidateDitects = new List<DetectionResult>();
-        // 使用する検出結果
+    
         List<DetectionResult> ditects = new List<DetectionResult>();
 
         for (int i = 0; i < outputWidth; i++) {
-            // 検出結果を解析
+            
             var result = new DetectionResult(output0, i);
-            // スコアが規定値未満なら無視
+          
             if (result.score < threshold) {
                 continue;
             }
-            // 候補として追加
+            
             candidateDitects.Add(result);
         }
 
-        // NonMaxSuppression処理
-        // 重なった矩形で最大スコアのものを採用
+        // NonMaxSuppression
         while (candidateDitects.Count > 0) {
             int idx = 0;
             float maxScore = 0.0f;
@@ -198,16 +190,16 @@ public class Main : MonoBehaviour {
                 }
             }
 
-            // score最大の結果を取得し、リストから削除
+            // score
             var cand = candidateDitects[idx];
             candidateDitects.RemoveAt(idx);
 
-            // 採用する結果に追加
+          
             ditects.Add(cand);
 
             List<int> deletes = new List<int>();
             for (int i = 0; i < candidateDitects.Count; i++) {
-                // IOUチェック
+                // IOU
                 float iou = Iou(cand, candidateDitects[i]);
                 if (iou >= iouThres) {
                     deletes.Add(i);
@@ -223,7 +215,7 @@ public class Main : MonoBehaviour {
 
     }
 
-    // 物体の重なり具合判定
+
     private float Iou(DetectionResult boxA, DetectionResult boxB) {
         if ((boxA.x1 == boxB.x1) && (boxA.x2 == boxB.x2) && (boxA.y1 == boxB.y1) && (boxA.y2 == boxB.y2)) {
             return 1.0f;
@@ -240,12 +232,12 @@ public class Main : MonoBehaviour {
     }
 
 
-    // 画像のリサイズ処理
+   
     private static Texture2D ResizedTexture(Texture2D texture, int width, int height) {
-        // RenderTextureに書き込む
+        
         var rt = RenderTexture.GetTemporary(width, height);
         Graphics.Blit(texture, rt);
-        // RenderTexgureから書き込む
+       
         var preRt = RenderTexture.active;
         RenderTexture.active = rt;
         var resizedTexture = new Texture2D(width, height);
@@ -268,7 +260,7 @@ public class Main : MonoBehaviour {
 
 }
 
-// 検出結果
+
 class DetectionResult {
     public float x1 { get; }
     public float y1 { get; }
@@ -278,8 +270,7 @@ class DetectionResult {
     public List<KeyPoint> keypoints { get; }
 
     public DetectionResult(Tensor t, int idx) {
-        // 検出結果で得られる矩形の座標情報は0:中心x, 1:中心y、2:width, 3:height
-        // 座標系を左上xy右下xyとなるよう変換
+        
         float halfWidth = t[0, 0, idx, 2] / 2;
         float halfHeight = t[0, 0, idx, 3] / 2;
         x1 = t[0, 0, idx, 0] - halfWidth;
@@ -288,7 +279,7 @@ class DetectionResult {
         y2 = t[0, 0, idx, 1] + halfHeight;
         score = t[0, 0, idx, 4];
 
-        // 各キーポイントのx,y,visibleが設定されている
+        
         int channels = t.shape.channels;
         keypoints = new List<KeyPoint>();
         for (int point = 5; point < channels; point+=3) {
@@ -302,7 +293,7 @@ class DetectionResult {
 
 }
 
-// 検出したキーポイント情報
+
 class KeyPoint {
     public float x { get; }
     public float y { get; }
